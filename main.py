e07ef7b222b33f1b35938f967f2074c069719b2e
Haider Abbasi <alih44974@gmail.com>
2022-08-13 10:43:08 +0530

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,66 @@
+# roll back when require in VSC
+# config.yml have templates which i can find on  hithub acces or circle ci
+# everything is available in docker image
+# to execute yml cmd we need machine, circle ci will provide that machine
+# circleci will use its machine & docker our application
+
+# for different platforms cicd pipeline will be different, like for azure, AWS, GCP & github access it
+# will be different
+# whenever we push this code to circleci , it will search for config.yml directory, & try to open this file
+# it will start reading its detail & execute every detail one by one
+# dockerfile will docker your image & it will allow your run your docker image
+# It can create your docker image to serve your application,
+# github access create the docker image & push it to some kind of server
+# github access detect that something has modified in our code, we have to run this config.yml file again
+# the instruction inside confif.yml , execute them one by one
+
+# docker file will not store docker image , it will store the steps to which are required to create the
+# docker image
+# config.yml is not a docker image, it is the instructions for circleci
+# circleci will read this file, for build & test, it will read, it will provide us machine
+# it will try to create env for python, it will test the application
+# it will use our existing docker file for docker image
+# we will provide our docker username & password, so that It will push our image which is created at circleci
+# those images will be pushed to docker hub, we'll be able to see dooker image to docker hub
+# In the deployment sector, It will take the existing image, & it   will deploy to heroku
+
+# cover code will be pushed to github, from github, we will configure circleci, this code (circleci.yml) is
+# available to circleci
+
+# circleci only check config.yml, inside that it will open the machine in circleci
+# Here it is to create venv, to upgrade pip & to install requirements.txt file
+# to test our application is running or not , we use test_script.py, here i am trying to access to route url
+# self.app.get('/') url, response 200 : our appl. is running
+# build the docker image, it will push this image to docker hub, it will login to dockerhub, than
+#  username than password
+# when docker image is creating at circleci
+# Test script is used for unit testing
+
+# Before work on project, writing your whole code: first test your application, than test your cicd pipeline
+# After than you can start implementing your actual code, than you don't need to worry about how you are
+# going to deploy your application
+# every you write your code, make the changes,  you will push your changes to github & automatically all the
+# changes will be reflected to the server, this is the benefit of establishing cicd pipeline in the first place
+
+# docker
+# docker build -t demo_flask:lastest .
+# docker ps
+# docker images
+# docker run -p 5000:5000 demo_flask:lastest
+# python3.6.2 is the machine for circleci where our docker image will be build, we don't need to worry about it
+
+# docker image will be available at docker hub
+# heroku will provide the container to run the docker image
+# you can run your image any where as long as you provide container to run it
+
+# ACR - for Azure, instead of circleci, ACR(Azure container registry), instead of docker hub,use azure kubernetes
+# ECS - for AWS, send your repo to Elastic container registry, you can read the images, deploy your images
+# to ECS (elastic container service)
+
+# docker file will give instructions to create docker image, config.yml have the instructions which circleci
+# will follow to create docker image, it will publish that image to docker hub, & it will publish that image to
+# heroku container
+
 #!/usr/bin/env python
 
 from wsgiref import simple_server
